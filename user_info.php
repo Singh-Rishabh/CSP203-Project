30cfa04f22bd285927884e0d9403d0b034311070
nithish001 <36620951+nithish001@users.noreply.github.com>
2018-04-23 15:14:43 +0530

    some changes in method

--- user_info.php
+++ user_info.php
@@ -1,5 +1,8 @@
 <?php
+    @ob_start();
 	session_start();
+?>
+<?php
 	$person_id = $_SESSION['person_id'];
 
 	$servername = "localhost";
@@ -83,79 +86,198 @@
         <script src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
         <script src="asset/js/script.js"></script>
 
+        <style>
+        .selcls { 
+            padding: 9px; 
+            border: solid 1px #517B97; 
+            outline: 0; 
+            background: -webkit-gradient(linear, left top, left 25, from(#FFFFFF), color-stop(4%, #CAD9E3), to(#FFFFFF)); 
+            background: -moz-linear-gradient(top, #FFFFFF, #CAD9E3 1px, #FFFFFF 25px); 
+            box-shadow: rgba(0,0,0, 0.1) 0px 0px 8px; 
+            -moz-box-shadow: rgba(0,0,0, 0.1) 0px 0px 8px; 
+            -webkit-box-shadow: rgba(0,0,0, 0.1) 0px 0px 8px; 
 
+            } 
+           
+        </style> 
     </head>
 	<body>
-        <div class="container-fluid">
+        <header class="clearfix">
+        
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="top-bar">
+                            <div class="row">
+                                    
+                                <div class="col-md-6">
+                                    <!-- Start Contact Info -->
+                                    <ul class="contact-details">
+                                        <li><a href="#"><i class="fa fa-phone"></i> +91-9592213703</a>
+                                        </li>
+                                        <li><a href="#"><i class="fa fa-envelope-o"></i> support@iitrpr.ac.in</a>
+                                        </li> 
+                                    </ul>
+                                    <!-- End Contact Info -->
+                                </div><!-- .col-md-6 -->
+                                
+                                <div class="col-md-6">
+                                </div>
+                            </div>
+                                
+                                
+                        </div>
+                    </div>                        
+
+                </div><!-- .row -->
+            </div><!-- .container -->
+            <!-- End Top Bar -->
+        
+            <!-- Start  Logo & Naviagtion  -->
+            <div class="navbar navbar-default navbar-top" >
+                <div class="container">
+                    <div class="navbar-header">
+                        <!-- Stat Toggle Nav Link For Mobiles -->
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                            <i class="fa fa-bars"></i>
+                        </button>
+                        <!-- End Toggle Nav Link For Mobiles -->
+                        <a class="navbar-brand" href="#"><img src="img/iitrprlogo.jpg" style="max-height: 40px;max-width: 40px; margin-top: -10px;"></a>
+                    </div>
+                    <div class="navbar-collapse collapse">
+              
+                        <!-- Start Navigation List -->
+                        <ul class="nav navbar-nav navbar-right">
+                            <li>
+                                <a href="index.html">Home</a>
+                            </li>
+                            <li>
+                                <a href="index.html#about-section">About Us</a>
+                            </li>
+                            <li>
+                                <a class="active"  href="portfolio.php">Library Management System</a>
+                            </li>
+                            <li>
+                                <a href="service.html">Student Activities</a>
+                            </li>
+                            <li>
+                                <a href="blog.html">Blog</a>
+<!--
+                                <ul class="dropdown">
+                                    <li>
+                                        <a href="blog-item.html">Item Page</a>
+                                    </li>
+                                </ul>
+-->
+                            </li>
+                            <li><a href="contact.html">Contact</a>
+                            </li>
+                        </ul>
+                        <!-- End Navigation List -->
+                    </div>
+                </div>
+            </div>
+            <!-- End Header Logo & Naviagtion -->
+            
+        </header>
+
+        <div class="page-header">
+            <div class="overlay">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-12 text-center">
+                            <h1>Search for users</h1>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid" style="padding:10px 30% 5%">
     		<form class="navbar-form navbar-center" method = "post">
-    			<div class="dropdown">
-    			  <button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Search by ** 
-    			  <span class="caret"></span></button>
-    			  <ul class="dropdown-menu">
-    			    <li><a href="?username">username</a></li>
-    			    <li><a href="?entry_number">Entry Number</a></li>
-                    <li><a href="?first_name">Name</a></li>
-    			  </ul>
+    			<select name="taskOption1" class="form-control selcls">
+                     <option value="default" selected>--Search By--</option>
+                     <option value="username">Username></option>
+                     <option value="first_name">Name</option>
+                     <option value="entry_number">Entry Number</option>
+                  </select>
+                  <select name="taskOption2" class="form-control selcls">
+                     <option value="default" selected>--Sort By--</option>
+                     <option value="Name">Name</option>
+                     <option value="second">Fine</option>
+                  </select>
     			  <input name="searchusers" type="text" id="searchusers" placeholder="Search users" class="form-control" style="border-color: #ccc;" required>
     			  <input type="submit" name ="searchuserBtn"  id="searchuserBtn" class="btn btn-default" value="Search" style="border-color: #ccc; margin-right: 15px;">
-    			</div>		  	
+    				  	
     		</form>
+            
         </div>
+
 
 		<?php
             //echo "Before all the isset<br\>";
             $result = null;
 			if(isset($_POST['searchuserBtn'])){
                // echo "button has been pressed <br\>";
-				if(isset($_GET['username'])){
+				if(isset($_POST['taskOption1'])){
                     //echo "username is active<br\>";
-					if(isset($_POST['searchusers'])){
-						$sql = "SELECT * from user_info inner join authenticate on user_info.person_id = authenticate.person_id where user_name like '%".$_POST['searchusers']."%'";
-                        //echo $sql;
-						$result = mysqli_query($conn,$sql);
+                    if($_POST['taskOption1']=="username"){
+    					if(isset($_POST['searchusers'])){
+    						$sql = "SELECT * from user_info inner join authenticate on user_info.person_id = authenticate.person_id where user_name like '%".$_POST['searchusers']."%'";
+                            //echo $sql;
+    						$result = mysqli_query($conn,$sql);
 
 
-					}
-				}
-				else if(isset($_GET['entry_number'])){
-                    if(isset($_POST['searchusers'])){
-                        $sql = "SELECT * from user_info inner join authenticate on user_info.person_id = authenticate.person_id where entry_number = \"".$_POST['searchusers']."\"";
-                        //echo $sql;
-                        $result = mysqli_query($conn,$sql);
+    					}
+    				}
+    				else if($_POST['taskOption1']=="entry_number"){
+                        if(isset($_POST['searchusers'])){
+                            $sql = "SELECT * from user_info inner join authenticate on user_info.person_id = authenticate.person_id where entry_number = \"".$_POST['searchusers']."\"";
+                            //echo $sql;
+                            $result = mysqli_query($conn,$sql);
+                        }
+
+
+    				}
+                    else if($_POST['taskOption1']=="first_name"){
+                        if(isset($_POST['searchusers'])){
+                            $sql = "SELECT * from user_info inner join authenticate on user_info.person_id = authenticate.person_id where first_name like '%".$_POST['searchusers']."%'";
+                            //echo $sql;
+                            $result = mysqli_query($conn,$sql);
+
+                        }   
+
                     }
 
+                    if(mysqli_num_rows($result)){
+                        echo "<div class=\"table-responsive\" style=\"margin:5%\">";
+                        echo "<table class=\"table\">";
+                        echo "<thead><tr>";
+                        $count = 0;
+                        echo "<th>#</th>";
+                        echo "<th>Firstname</th>";
+                        echo "<th>Lastname</th>";
+                        echo "<th>Link to Profile</th></tr></thead><tbody>";
+                        while($row = mysqli_fetch_assoc($result)){
+                            $count = $count + 1;
+                            echo "<tr>
+                                    <td>".$count."</td>".
+                                    "<td>".$row['first_name']."</td>".
+                                    "<td>".$row['last_name']."</td>".
+                                    "<td><a href=\"?userID=".$row['user_name']."\">".
+                                        "For more information".
+                                    "</a>
+                                    </td>
+                                </tr>";        
+                        }
+                        echo "</tbody></ul></table></div>";
+                    }
 
-				}
-                else if(isset($_GET['first_name'])){
-                    if(isset($_POST['searchusers'])){
-                        $sql = "SELECT * from user_info inner join authenticate on user_info.person_id = authenticate.person_id where first_name like '%".$_POST['searchusers']."%'";
-                        //echo $sql;
-                        $result = mysqli_query($conn,$sql);
+                    else{
 
-                    }   
-
+                        echo "<h2 style=\"text-align:center;margin:5%\"> No Results Found.</h2>";
+                    }
                 }
-                echo "<div class=\"table-responsive\">";
-                echo "<table class=\"table\">";
-                echo "<thead><tr>";
-                $count = 0;
-                echo "<th>#</th>";
-                echo "<th>Firstname</th>";
-                echo "<th>Lastname</th>";
-                echo "<th>Link to Profile</th></tr></thead><tbody>";
-                while($row = mysqli_fetch_assoc($result)){
-                    $count = $count + 1;
-                    echo "<tr>
-                            <td>".$count."</td>".
-                            "<td>".$row['first_name']."</td>".
-                            "<td>".$row['last_name']."</td>".
-                            "<td><a href=\"?".$row['user_name']."\">".
-                                "For more information".
-                            "</a>
-                            </td>
-                        </tr>";        
-                }
-                echo "</tbody></ul></div>";
 
 
 			}
@@ -165,16 +287,117 @@
                 
             $query_string = parse_url($actual_link, PHP_URL_QUERY);
             //echo $query_string."<br>";
-            if(strcmp($query_string,"") and strcmp($query_string,"first_name") and strcmp($query_string,"entry_number") and strcmp($query_string,"username")){
-                
-                    echo "<script>alert('Redirecting the page');</script>";
-                    $_SESSION['userID1'] = $query_string;
-                    header("Refresh: 0; url=userProfile.php");
-
-                
-
+            if(isset($_GET['userID'])){
+                if(strcmp($_GET['userID'],"")){
+                        echo "<script>alert('Redirecting the page');</script>";
+                        $_SESSION['userID1'] = $_GET['userID'];
+                        header("Location:userProfile.php");
+                }
             }
 
+
 		?>
+
+        <section id="footer-section" class="footer-section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3">
+                        <div class="section-heading-2">
+                            <h3 class="section-title">
+                                <span>Address</span>
+                            </h3>
+                        </div>
+                        
+                        <div class="footer-address">
+                            <ul>
+                                <li class="footer-contact"><i class="fa fa-home"></i>IIT Roapr Nangal Road, Punjab</li>
+                                <li class="footer-contact"><i class="fa fa-envelope"></i><a href="#">support@iitrpr.ac.in</a></li>
+                                <li class="footer-contact"><i class="fa fa-phone"></i>+91-9592213703</li>
+                                <li class="footer-contact"><i class="fa fa-globe"></i><a href="http://www.iitrpr.ac.in/" target="_blank">http://www.iitrpr.ac.in/</a></li>
+                            </ul>
+                        </div>
+                    </div><!--/.col-md-3 -->
+                    
+                    
+                    <div class="col-md-3">
+                        <div class="section-heading-2">
+                            <h3 class="section-title">
+                                <span>Latest Tweet</span>
+                            </h3>
+                        </div>
+                        
+                        <div class="latest-tweet">
+                            <div class="media">
+                                <div class="media-left">
+                                    <i class="fa fa-twitter fa-2x media-object"></i>
+                                </div>
+                                <div class="media-body">
+                                    <h4 class="media-heading">About 15 days ago</h4>
+                                    <p>xyz xyz xyz xyz xyz xyz xyz xyz xyz xyz xyz xyz xyz xyz xyz xyz xyz xyz xyz xyz xyz xyz xyz xyz</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!--/.col-md-3 -->
+                    
+                    <div class="col-md-3">
+                        <div class="section-heading-2">
+                            <h3 class="section-title">
+                                <span>Stay With us</span>
+                            </h3>
+                        </div>
+                        <div class="subscription">
+                            <p>Some Text Some Text Some Text Some Text Some Text Some Text Some Text</p>
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Your E-mail" id="name" required data-validation-required-message="Please enter your name.">
+                                <input type="submit" class="btn btn-primary" value="Subscribe">
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="col-md-3">
+                        <div class="section-heading-2">
+                            <h3 class="section-title">
+                                <span>Images</span>
+                            </h3>
+                        </div>
+                        
+                        <div class="flickr-widget">
+                            <ul class="flickr-list">
+                                <li>
+                                    <a href="img/iitrprlogo.jpg" data-lightbox="picture-1">
+                                        <img src="img/iitrprlogo.jpg" alt="logo" class="img-responsive">
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="img/iitrpr.jpg" data-lightbox="picture-2">
+                                        <img src="img/iitrpr.jpg" alt="IITrpr" class="img-responsive">
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="img/rishabh.jpg" data-lightbox="picture-3">
+                                        <img src="img/rishabh.jpg" alt="Rishabh" class="img-responsive">
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="img/sunny.jpg" data-lightbox="picture-4">
+                                        <img src="img/sunny.jpg" alt="Nithish" class="img-responsive">
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="img/jagadeesh.jpg" data-lightbox="picture-5">
+                                        <img src="img/jagadeesh.jpg" alt="" class="img-responsive">
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="img/vineel.jpg" data-lightbox="picture-6">
+                                        <img src="img/vineel.jpg" alt="" class="img-responsive">
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div><!--/.col-md-3 -->
+                </div><!--/.row -->
+            </div><!-- /.container -->
+        </section>
 	</body>
 </html>
